2ed28699696a8e9558a1e00a2c806058a9188a0a
Christian Aichner <contact@aichner-christian.com>
2019-03-08 09:24:03 +0100

    add html support for section 0

--- src/components/organisms/SectionContents/0.js
+++ src/components/organisms/SectionContents/0.js
@@ -17,19 +17,19 @@ const SectionContent = (props: Props): React.Element<*> => {
                 <div className="col-md-4">
                     <div className="row">
                         <div className="col-12"><img draggable="false" className="img-fluid" src={content[1].icon} alt="Search Icon"/></div>
-                        <div className="col-12">{content[1].text}</div>
+                        <div className="col-12" dangerouslySetInnerHTML={{__html: content[1].text}}></div>
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="row">
                         <div className="col-12"><img draggable="false" className="img-fluid" src={content[2].icon} alt="Expert Icon"/></div>
-                        <div className="col-12">{content[2].text}</div>
+                        <div className="col-12" dangerouslySetInnerHTML={{__html: content[2].text}}></div>
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="row">
                         <div className="col-12"><img draggable="false" className="img-fluid" src={content[3].icon} alt="Leaf Icon"/></div>
-                        <div className="col-12">{content[3].text}</div>
+                        <div className="col-12" dangerouslySetInnerHTML={{__html: content[3].text}}></div>
                     </div>
                 </div>
             </div>
