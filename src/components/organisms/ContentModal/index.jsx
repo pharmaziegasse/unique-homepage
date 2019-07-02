252bf4d30120aa3a60baaca5e8218edcd2fd2241
Christian Aichner <contact@aichner-christian.com>
2019-07-02 12:29:17 +0200

    fix Modal call issue

--- src/components/organisms/ContentModal/index.jsx
+++ src/components/organisms/ContentModal/index.jsx
@@ -3,28 +3,24 @@ import * as React from 'react'
 class ContentModal extends React.Component{
     constructor(props){
         super(props);
-        this.state = {show: true}
     }
 
     render() {
-        console.log(this.props);
-        if(this.state.show){
-            return (
-                <div className="modal fade" id={this.props.modaldata[0].modalId} tabIndex="-1" role="dialog" aria-labelledby={this.props.modaldata[0].modalLabel} aria-hidden="true" data-backdrop="true">
-                    <div className="modal-dialog modal-lg modal-notify modal-info" role="document">
-                        <div className="modal-content">
-                            <div className="modal-body">
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true" className="dark-text">×</span>
-                                </button>
-                                <div className="lead font-weight-bold text-center" dangerouslySetInnerHTML={{__html: this.props.modaldata[0].modalTitle}}></div>
-                                {this.props.children}
-                            </div>
+        return (
+            <div className="modal fade" id={this.props.modaldata[0].modalId} tabIndex="-1" role="dialog" aria-labelledby={this.props.modaldata[0].modalLabel} aria-hidden="true" data-backdrop="true">
+                <div className="modal-dialog modal-lg modal-notify modal-info" role="document">
+                    <div className="modal-content">
+                        <div className="modal-body">
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" className="dark-text">×</span>
+                            </button>
+                            <div className="lead font-weight-bold text-center" dangerouslySetInnerHTML={{__html: this.props.modaldata[0].modalTitle}}></div>
+                            {this.props.children}
                         </div>
                     </div>
                 </div>
-            )
-        }
+            </div>
+        )
     }
 }
 
