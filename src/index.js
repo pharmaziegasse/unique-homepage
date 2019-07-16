631a132a8e33d7d2da4225ec8aac2bd97076a2b0
Christian Aichner <contact@aichner-christian.com>
2019-07-16 10:07:14 +0200

    add shop redirect

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if(window.location.pathname === "/shop"){
+  window.location.replace('https://pharmaziegasse.weeblysite.com')
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
